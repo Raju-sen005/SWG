bd03c0da5ebc483c0ce0c6c4350ea4869495bf17
Raju_0005 <rajugbp2006@gmail.com>
2024-12-30 17:22:37 +0530

    Add files via upload

---
+++ 30.js
@@ -0,0 +1,27 @@
+let s = ["S", "W", "G"];
+let random = Math.floor(Math.random() * s.length);
+let cpu = s[random];
+console.log(cpu);
+// let runAg;
+// while(runAg){
+	let game = prompt("Choose one from S,W and G.");
+// }
+const swg = (user, cpu) => {
+	if (user === cpu) {
+		return "Draw";
+	} else if (
+		(user === "S" && cpu === "W") ||
+		(user === "W" && cpu === "G") ||
+		(user === "G" && cpu === "S")
+	) {
+		return "You win!";
+	} else {
+		return "CPU win!";
+	}
+};
+// runAg= alert("play again!")
+	let result = swg(game, cpu);
+	console.log(result);
+
+let aft = "Congratulations! you win the game."
+document.write(aft)
